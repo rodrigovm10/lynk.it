6703f7fe657bcaf87fc84b7939aeaa687ecb30d3
rodrigovm0420 <rvega@becompliance.es>
2025-02-05 21:02:19 -0600

    Feature: Add dashboard filter and options

--- app/dashboard/links/page.tsx
+++ app/dashboard/links/page.tsx
@@ -1,3 +1,32 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Plus, Search, Tag } from 'lucide-react'
+
 export default function Links() {
-  return <div>hola</div>
+  return (
+    <>
+      <section className='flex justify-between gap-6'>
+        <div className='relative'>
+          <Search
+            className='absolute z-1 left-2 top-3'
+            size={16}
+          />
+          <Input
+            className=' pl-8 text-sm'
+            placeholder='Search a link'
+          />
+        </div>
+        <div className='flex gap-4'>
+          <Button variant='outline'>
+            <Tag />
+            <span className='font-semibold'>Select tag</span>
+          </Button>
+          <Button>
+            <Plus />
+            <span className='font-semibold'>Create link</span>
+          </Button>
+        </div>
+      </section>
+    </>
+  )
 }
