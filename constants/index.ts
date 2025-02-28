298eb426a822b0400ce7a7dcfb3fca50f95382e7
rodrigovm0420 <rvega@becompliance.es>
2025-02-28 16:15:59 -0600

    fix: Modify env variables

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
 import { Tables } from '@/types'
 
-export const LYNK_TO_COPY = (lynk: Tables<'lynks'>) => `http://localhost:3000/${lynk.lynk}`
+export const LYNK_TO_COPY = (lynk: Tables<'lynks'>) => `${process.env.SITE_URL!}/${lynk.lynk}`
